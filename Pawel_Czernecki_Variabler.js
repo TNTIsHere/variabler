83e43277afed6c721078116b05908fffb8cb3831
TNTIsHere <pawelczernecki839@gmail.com>
2022-03-30 09:31:49 +0200

    variabler har blitt addet men jeg har ogs en pistol mot hode av ilja semjonovs

---
+++ Pawel_Czernecki_Variabler.js
@@ -0,0 +1,29 @@
+//Oppgave 1
+
+//Å deklarere en variabel betyr å lage en variabel og gi den en verdi.
+
+let variabelNavn1 = 123;
+const variabelNavn2 = 'hei';
+
+//Oppgave 2
+
+//Å intialisere en variabel betyr å bruke den i koden ved å endre verdien eller å skrive den ut med console.log for eksempel.
+
+console.log(variabelNavn1);
+
+
+//Oppgave 3
+
+let iljaErCute = "søt"; console.log(iljaErCute);
+
+//Oppgave 4
+
+//Forskjellen mellom "var" og "let" er at var deklarerer en "function-scoped" variabel mens "let" deklarerer en "block-scoped" funksjon. Dette betyr at let blir hoistet mens var blir ikke det. "Var" er uttdatert og er definert som "disencouraged".
+
+//Oppgave 5
+
+//En konstant eller "const" kan ikke endres i verdi eller definisjon. Du kan bare deklarere en konst en gang. Mens en variabel kan "variere" i sin verdi så mange ganger den vil.
+
+//Oppgave 6
+
+//I javascript finnes det 3 forskjellige grunnleggende datatyper. Det finnes number, string og object. Boolean er også en datatype som gir enten 1 eller 0. 1 = True, 0 = False. BigInt er brukt for å regne med veldig høye tall.
